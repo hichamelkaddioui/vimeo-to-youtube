bf4b628bb8b47416b41661624ecd24a8952e36c7
Hicham <hicham.elkaddioui+git@gmail.com>
2018-08-21 11:31:15 +0200

    feat: add interactive UI
    The app now prompts nice questions.

---
+++ app.js
@@ -0,0 +1,35 @@
+'use strict'
+
+const inquirer = require('inquirer')
+
+const logger = require('./lib/logger')
+const dl = require('./lib/download')
+const util = require('./lib/util')
+
+logger.info('Starting app')
+
+const CHOICE_PRINT_LIST = '🖨 Print the list of videos'
+const CHOICE_DOWNLOAD = '⏬ Download videos from Vimeo'
+const CHOICE_UPLOAD = '⏫ Upload videos to YouTube'
+
+const start = () =>
+  inquirer.prompt({
+    type: 'list',
+    name: 'action',
+    message: 'What do you want to do?',
+    choices: [CHOICE_PRINT_LIST, CHOICE_DOWNLOAD, CHOICE_UPLOAD]
+  })
+    .then(answer => {
+      logger.debug('Question answered', answer)
+
+      switch (answer.action) {
+        case CHOICE_DOWNLOAD:
+          return util.fetchVideosIfDatabaseIsEmpty().then(() => dl())
+        case CHOICE_PRINT_LIST:
+          break
+        case CHOICE_UPLOAD:
+        default:
+      }
+    })
+
+module.exports = { start }
--- index.js
+++ index.js
@@ -1,37 +1,5 @@
 'use strict'
 
-const logger = require('./lib/logger')
-const vimeo = require('./lib/vimeo')
-const dl = require('./lib/download')
-const db = require('./lib/database')
+const app = require('./app')
 
-logger.info('Starting app')
-
-/**
- * Gets the list of videos from the database, or fetches it from Vimeo first if
- * the database is empty.
- *
- * @async
- * @param {Boolean} isEmpty - Whether the database is empty
- * @return {Promise<Object[]>} The list of videos
- */
-const fetchVideosIfDatabaseIsEmpty = isEmpty => {
-  if (!isEmpty) {
-    logger.info('Database is not empty, using local values')
-
-    return db.findAsync({})
-  }
-
-  logger.info('Database is empty, fetching all videos from Vimeo')
-
-  return vimeo.fetchAllVideos()
-    .then(videos => Promise.all(videos.map(video => db.insertVideoIfNotExistsAsync(video))))
-}
-
-const start = () =>
-  db.isEmptyAsync()
-    .then(isEmpty => fetchVideosIfDatabaseIsEmpty(isEmpty))
-    .then(() => dl())
-    .catch(err => logger.error(err.message))
-
-start()
+app.start()
