535fc362c1c5e1e55a16e3b1d3ad150d2172bb28
Hicham <hicham.elkaddioui+git@gmail.com>
2018-08-22 13:58:00 +0200

    refactor: move methods outside of main 'app.js'

--- app.js
+++ app.js
@@ -5,36 +5,34 @@ const inquirer = require('inquirer')
 const logger = require('./lib/logger')
 const dl = require('./lib/download')
 const ul = require('./lib/upload')
-const vimeo = require('./lib/vimeo')
-const { databaseIsEmpty, findAll, insertIfNotExists, resetDownloadingVideos } = require('./lib/database')
+const { fetchAndInsert, fetchIfDatabaseIsEmpty } = require('./lib/vimeo')
+const { resetDownloadingVideos } = require('./lib/database')
 const { printUtil } = require('./lib/util')
 
 logger.info('App started')
 
-const CHOICE_PRINT_LIST = '🖨 Print the list of videos'
-const CHOICE_RESET_DOWNLOADING = '🔄 Reset videos marked as \'downloading\''
-const CHOICE_DOWNLOAD = '⏬ Download videos from Vimeo'
-const CHOICE_UPLOAD = '⏫ Upload videos to YouTube'
-const CHOICE_QUIT = '💣 Quit'
-
-const fetchVideosIfDatabaseIsEmpty = () => databaseIsEmpty()
-  .then(databaseIsEmpty => {
-    if (!databaseIsEmpty) {
-      logger.info('Using database values')
-
-      return findAll()
-    }
-
-    logger.info('Database is empty, fetching all videos from Vimeo')
-
-    return vimeo.fetchAllVideos().then(videos => Promise.all(videos.map(video => insertIfNotExists(video))))
-  })
+const CHOICE_DOWNLOAD = '⇓ Download videos from Vimeo'
+const CHOICE_UPLOAD = '⇑ Upload videos to YouTube'
+const CHOICE_PRINT_LIST = '⎙ Print the list of videos'
+const CHOICE_REFRESH = '⇣ Refresh the list of videos from Vimeo'
+const CHOICE_RESET_DOWNLOADING = '⟳ Reset videos marked as \'downloading\''
+const CHOICE_QUIT = '☠ Quit'
 
 const prompt = () => inquirer.prompt({
   type: 'list',
   name: 'action',
   message: 'What do you want to do?',
-  choices: [CHOICE_PRINT_LIST, CHOICE_RESET_DOWNLOADING, CHOICE_DOWNLOAD, CHOICE_UPLOAD, CHOICE_QUIT]
+  choices: [
+    CHOICE_DOWNLOAD,
+    CHOICE_UPLOAD,
+    new inquirer.Separator(),
+    CHOICE_PRINT_LIST,
+    CHOICE_REFRESH,
+    CHOICE_RESET_DOWNLOADING,
+    new inquirer.Separator(),
+    CHOICE_QUIT,
+    new inquirer.Separator()
+  ]
 })
   .then(answer => {
     logger.debug('Question answered', answer)
@@ -46,10 +44,11 @@ const prompt = () => inquirer.prompt({
         .then(n => logger.info(`${n} video(s) reset`))
         .then(() => prompt())
       case CHOICE_PRINT_LIST: return printUtil.all().then(() => prompt())
+      case CHOICE_REFRESH: return fetchAndInsert().then(() => prompt())
       default:
     }
   })
 
-const start = () => fetchVideosIfDatabaseIsEmpty().then(() => prompt())
+const start = () => fetchIfDatabaseIsEmpty().then(() => prompt())
 
 module.exports = { start }
